197a5d6373d0c2f28b1882c7bf359245d88e2b47
ramprakash-nita <ramprakashwithzeal@gmail.com>
2025-08-24 15:44:36 +0530

    fix added 1 failure test and added multiply feature,

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,11 @@
-from src.main import add
+from src.main import add, multiply
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
-    assert add(0, 0) == 1
+    assert add(0, 0) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 5) == 0
